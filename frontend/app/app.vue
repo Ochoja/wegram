7c2e8cc560aa6686af64b7b63826f31bc8257a00
Ochoja <danielochoja@yahoo.com>
2025-09-12 03:58:31 +0100

    added tailwind support

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex min-h-screen bg-[#0A0F1D] text-[#E6ECFF]">
+    <Sidebar class="hidden md:block" />
+    <main class="flex-1 p-4">
+      <NuxtPage />
+    </main>
+    <Dock class="md:hidden" />
   </div>
 </template>
+
+<script setup>
+import Sidebar from '@/components/Sidebar.vue';
+import Dock from '@/components/Dock.vue';
+</script>
