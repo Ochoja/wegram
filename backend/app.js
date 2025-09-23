e4f7c03ab2aa5429329f93d85976d70d9a8bdd1e
chisomdaniel <chinwezechisom@gmail.com>
2025-09-23 11:29:41 +0100

    fix: update OAuth provider to 'twitter' in user model and passport configuration
    Signin with Twitter now works as expected

--- backend/app.js
+++ backend/app.js
@@ -15,14 +15,14 @@ const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase';
 
 app.use(cors());
-
 app.use(passport.initialize());
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   cookie: { secure: process.env.NODE_ENV === 'production' }
-}))
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,7 +32,6 @@ app.use(express.json({ limit: '10mb' }));
 // routes
 app.use('/api/v1/posts', postsRouter);
 app.use('/api/auth', authRouter);
-
 
 mongoose.set('strictQuery', false);
 mongoose.connect(MONGODB_URI)
